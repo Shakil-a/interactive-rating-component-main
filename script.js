13b67261a5ffb7b78bebe61dedeb998085952aa0
Shakil Ahmed <ahmed1shakil@gmail.com>
2024-08-06 14:16:13 +0100

    pseduocode  of the algorithm for the logic

---
+++ script.js
@@ -0,0 +1,10 @@
+// the algorithm is :
+
+/* 
+1. get the submit button through a document query
+2. get all the buttons with a document query all
+3. create an empty variable to store the value of the button the user clicks
+4. have an event listener to each button and if its clicked the value is stored in the empty variable
+5. we then want an event listener on the submit button on if theres a value we want to redirect to the success page with the users rating
+included in the success message and if not we want an error message saying to pick a rating first.
+*/
