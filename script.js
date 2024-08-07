11020c86afabf75ad2166b97c27618f9fe0f71b7
Shakil Ahmed <ahmed1shakil@gmail.com>
2024-08-07 21:10:39 +0100

    redirected to success page and created error message

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const submitBtn = document.querySelector('#submitBtn');
     const numButtons = document.querySelectorAll('.numBtn');
     let btnClickedValue = 0;
+    const errorMessage = document.querySelector('#error-message');
     
     // 4. have an event listener to each button and if its clicked the value is stored in the empty variable
         numButtons.forEach((button) => {
@@ -14,5 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
         })
 
     // 5. we then want an event listener on the submit button on if theres a value we want to redirect to the success page with the users rating
-    // included in the success message and if not we want an error message saying to pick a rating first.
+    submitBtn.addEventListener('click', () => {
+        if (btnClickedValue){
+            window.location.href = "successPage.html"
+        }
+
+        // included in the success message and if not we want an error message saying to pick a rating first.
+        else {
+           errorMessage.innerHTML = "Please pick a rating first"
+        }
+    })
 })
