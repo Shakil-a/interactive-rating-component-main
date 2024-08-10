3c4c7ca83192365cb2b3ab39f6bb4aa5d22a035a
Shakil Ahmed <ahmed1shakil@gmail.com>
2024-08-10 12:13:31 +0100

    styled the success page and also redirect back to index

--- success.js
+++ success.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const rating = localStorage.getItem('selectedRating');
     const ratingPara = document.querySelector('#selectedRatingPara');
+    const body = document.querySelector('body');
     
     if(rating){
         ratingPara.textContent = `You selected ${rating} out of 5`
@@ -8,4 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
     else{
         alert('please select a rating')
     }
+
+    body.addEventListener('click', () => {
+        window.location.href = "index.html";
+        
+    })
 })
+
